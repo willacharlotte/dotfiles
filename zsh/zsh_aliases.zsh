6cbecea466fc453aa8234dee1cc2e674152153f8
Willa <wlyle98@gmail.com>
2023-12-27 23:07:35 +0200

    feat: remove vivi alias and add nv and vim aliases linking to nvim

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,8 +1,9 @@
-alias vivi="vim $MYVIMRC"
 alias cdz="cd $ZDOTDIR"
 alias srcz="clear && source $ZDOTDIR/.zshrc"
 alias l="ls -hAl"
 alias gits="git status"
 alias gitd="git diff"
 alias clip="xclip -r -selection c"
+alias nv="nvim"
+alias vim="nvim"
 
