2b25c398fb0af5eb2c1f6acdb545a9f21f902a2d
Willa <wlyle98@gmail.com>
2023-12-28 14:47:55 +0200

    feat: add is git var to zgp

--- zsh/plugins/zsh-git-parse/zsh-git-parse.zsh
+++ zsh/plugins/zsh-git-parse/zsh-git-parse.zsh
@@ -1,17 +1,26 @@
-# MY FIRST ATTEMPT AT A PLUGIN LMAO
+# MY FIRST ATTEMPT AT A PLUGIN LMAO BE GENTLE
+
+# declare ZGP vars
+declare -x ZGP_IS_GIT
+declare -x ZGP_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+declare -ix ZGP_STATUS_UNTRACKED=0
+declare -ix ZGP_STATUS_STAGED=0
+declare -ix ZGP_STATUS_UNSTAGED=0
+
+# assign ZGP_IS_GIT a value
+git rev-parse --is-inside-work-tree > /dev/null 2> /dev/null
+if [[ $? -eq 0 ]]; then
+  ZGP_IS_GIT=true
+else
+  ZGP_IS_GIT=false
+fi
 
 # parse porcelain status into STATUSES array
 declare STATUS_RAW=$(git status --porcelain 2> /dev/null)
 IFS=$'\n' read -rd '' -A STATUSES <<<"$STATUS_RAW"
 unset STATUS_RAW
 
-# define ZGP vars
-declare -x ZGP_BRANCH=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
-declare -ix ZGP_STATUS_UNTRACKED=0
-declare -ix ZGP_STATUS_STAGED=0
-declare -ix ZGP_STATUS_UNSTAGED=0
-
-# populate ZGP vars
+# calculate ZGP_STATUS_* values 
 for STATUS in "${STATUSES[@]}";
 do
   local CODE="${STATUS::2}"
@@ -28,12 +37,15 @@ done
 unset STATUSES
 
 # for debugging purposes
+# echo "is git: $ZGP_IS_GIT"
 # echo "branch: $ZGP_BRANCH"
 # echo "untracked: $ZGP_STATUS_UNTRACKED"
 # echo "staged: $ZGP_STATUS_STAGED"
 # echo "unstaged: $ZGP_STATUS_UNSTAGED"
 
 # TODO unset vars in zsh closing function thing
+# unset ZGP_IS_GIT
+# unset ZGP_BRANCH
 # unset ZGP_STATUS_UNTRACKED
 # unset ZGP_STATUS_STAGED
 # unset ZGP_STATUS_UNSTAGED
