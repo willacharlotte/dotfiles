3a1031180eaf1d0a80cc004c11d8e6ff83bb08f7
Willa <wlyle98@gmail.com>
2023-12-30 06:52:12 +0200

    feat: ls auto color

--- zsh/zsh_aliases.zsh
+++ zsh/zsh_aliases.zsh
@@ -1,9 +1,11 @@
 alias cdz="cd $ZDOTDIR"
 alias srcz="clear && source $ZDOTDIR/.zshrc"
+alias ls="ls --color=auto"
 alias l="ls -hAl"
 alias gits="git status"
 alias gitd="git diff"
 alias clip="xclip -r -selection c"
 alias nv="nvim"
 alias vim="nvim"
+alias jc="jack_control"
 
